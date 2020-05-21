30d012aabe6eb3efee69e87688433097eb5bd1f1
prasertcbs <bluecotton.k@gmail.com>
2020-05-21 20:13:13 +0700

    Add files via upload

--- disney_db.sql
+++ disney_db.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `disney` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `disney`;
 -- MySQL dump 10.13  Distrib 8.0.20, for Linux (x86_64)
 --
 -- Host: localhost    Database: disney
@@ -152,4 +154,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-05-20  9:31:44
+-- Dump completed on 2020-05-21 20:10:48
